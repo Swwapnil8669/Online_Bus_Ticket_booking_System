3ffb4be7f832be431cab1f76d8f406044a928dec
Sameer Kadu <Samirkadu8@gmail.com>
2024-12-15 23:58:20 +0530

    Complete with operator registration details

--- go_bus_frontend/src/App.js
+++ go_bus_frontend/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// import { Navbar } from "./View/nvabar";
+import { OperatorDetails } from "./View/operator/operator_details";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="operator/Details" element ={<OperatorDetails/>}/>
+    </Routes>
+     
+    
   );
 }
 
