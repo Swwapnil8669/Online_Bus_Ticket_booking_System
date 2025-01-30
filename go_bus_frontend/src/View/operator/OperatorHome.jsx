7497f985e20e1332606423ae96dd9eb819bb2006
kasar-akankasha <kasarakankasha@gamil.com>
2025-01-30 17:20:24 +0530

    Opeataor home page

--- go_bus_frontend/src/View/operator/OperatorHome.jsx
+++ go_bus_frontend/src/View/operator/OperatorHome.jsx
@@ -3,11 +3,24 @@ import './OperatorHome.css';
 
 function OperatorHome() {
   return (
-    <div className="operator-home">
+    <div>
+      
+      <div className="operator-home">
       <img src= "./img/bus.jpg" alt="Bus" className="background-image" /> 
+      <div className="overlay" style={{width:"100px", paddingLeft:"90%"}}>
+      <a href="#" class="btn btn-secondary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Logout</a>
+      </div>
       <div className="overlay"> 
         <img src="./img/GoBuslogo.png" alt="Logo" className="logo" />
-        <button className="enquire-button">logout</button>
+        <div className='linkes'>
+         <table>
+          <tr>
+            <td><a href=''></a>AddBus</td>
+            <td><a href=''></a>BusShedule</td>
+          </tr>
+         </table>
+        </div>
+      </div>
       </div>
     </div>
   );
