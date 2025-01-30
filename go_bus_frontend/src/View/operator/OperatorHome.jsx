e71d2d6e58f6de11a69991b0c010777dbec4ab88
kasar-akankasha <kasarakankasha@gamil.com>
2025-01-30 18:07:11 +0530

    half operater done

--- go_bus_frontend/src/View/operator/OperatorHome.jsx
+++ go_bus_frontend/src/View/operator/OperatorHome.jsx
@@ -9,16 +9,15 @@ function OperatorHome() {
       <img src= "./img/bus.jpg" alt="Bus" className="background-image" /> 
       <div className="overlay" style={{width:"100px", paddingLeft:"90%"}}>
       <a href="#" class="btn btn-secondary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Logout</a>
+      <ul style={{paddingRight:"20px"}}>
+            <li><a href="" target='t1'>AddBus</a></li>
+            <li><a href="" target='t1'>Bus Shedule</a></li>
+          </ul>
       </div>
       <div className="overlay"> 
         <img src="./img/GoBuslogo.png" alt="Logo" className="logo" />
-        <div className='linkes'>
-         <table>
-          <tr>
-            <td><a href=''></a>AddBus</td>
-            <td><a href=''></a>BusShedule</td>
-          </tr>
-         </table>
+        <div id="d2" style={{backgroundColor:"white",height:"1050px"}}>
+            <iframe id="t1" name='t1' height="100%" width="100%"></iframe>
         </div>
       </div>
       </div>
