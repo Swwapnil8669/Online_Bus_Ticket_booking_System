be99bad3360894a68596c68a1c9fea5e1ce0b5a2
Durgesh6403 <durgeshahire6403@gmail.com>
2025-02-09 20:15:51 +0530

    Delete go_bus_backend directory

--- go_bus_backend/go_bus_backend/src/main/java/com/bus/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.bus.exception;
-
-@SuppressWarnings("serial")
-public class ResourceNotFoundException extends RuntimeException{
-
-	public ResourceNotFoundException() {
-		super("resource not found");
-	}	
-}
